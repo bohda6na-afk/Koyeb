e724b62d6a855ead5a8c49f3e0228a75942b0720
Arsenii Stratiuk <stratuik.777@gmail.com>
2025-04-08 17:28:40 +0300

    A minor change in HTML

--- wartrace/wartrace/settings.py
+++ wartrace/wartrace/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-%ronxm3e#_3$6e4xrqkpe*g=yp%pbx%b506ohh05mr33&98q2n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.10.224.181',
+                 '127.0.0.1',
+]
 
 
 # Application definition
